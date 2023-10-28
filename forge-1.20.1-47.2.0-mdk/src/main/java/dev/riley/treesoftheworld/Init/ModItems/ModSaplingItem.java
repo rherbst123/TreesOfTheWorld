fba37df7ec456adb7cc087b660658810e5b07eed
rileyherbst <rileyherbst@yahoo.com>
2023-10-27 20:04:04 -0500

    SaplingModItem

---
+++ forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/treesoftheworld/Init/ModItems/ModSaplingItem.java
@@ -0,0 +1,10 @@
+package dev.riley.treesoftheworld.Init.ModItems;
+
+import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.SaplingBlock;
+
+public class ModSaplingItem extends BushBlock {
+    public ModSaplingItem(Properties properties){
+        super(properties);
+    }
+}
