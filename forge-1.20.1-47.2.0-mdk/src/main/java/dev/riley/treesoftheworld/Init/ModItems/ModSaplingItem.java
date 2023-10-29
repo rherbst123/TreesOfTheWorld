af7370f486390bb989031688c60a83d5c0b2c1cc
rileyherbst <rileyherbst@yahoo.com>
2023-10-29 10:56:49 -0500

    sapling stuff

--- forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/treesoftheworld/Init/ModItems/ModSaplingItem.java
+++ forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/treesoftheworld/Init/ModItems/ModSaplingItem.java
@@ -1,12 +1,18 @@
 package dev.riley.treesoftheworld.Init.ModItems;
 
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.SaplingBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
 
-public class ModSaplingItem extends BushBlock {
+public class ModSaplingItem extends BushBlock  {
     public ModSaplingItem(Properties properties){
         super(properties);
     }
 
-
+    @Override
+    public boolean canBeReplaced(BlockState p_60535_, Fluid p_60536_) {
+        return super.canBeReplaced(p_60535_, p_60536_);
+    }
 }
