83220dd42307363d37be9d7b2e05786a3cfad7cf
Riley Herbst <57201041+rherbst123@users.noreply.github.com>
2023-11-26 18:30:38 -0600

    Maple Trees Done! V1.1

--- forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/treesoftheworld/Init/ModItems/ModConfiguredFeatures.java
+++ forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/treesoftheworld/Init/ModItems/ModConfiguredFeatures.java
@@ -1,19 +1,27 @@
 package dev.riley.treesoftheworld.Init.ModItems;
 
+import com.google.common.collect.ImmutableList;
 import dev.riley.treesoftheworld.Init.BlockInit;
 import dev.riley.treesoftheworld.TreesOfTheWorld;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.MegaPineFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.treedecorators.AlterGroundDecorator;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.DarkOakTrunkPlacer;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.GiantTrunkPlacer;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 
 public class ModConfiguredFeatures {
@@ -25,12 +33,13 @@ public class ModConfiguredFeatures {
     public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
         register(context, MAPLE, Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                 BlockStateProvider.simple(BlockInit.MAPLE_LOG.get()),
-                new StraightTrunkPlacer(8, 5, 4),
+                //new StraightTrunkPlacer(7, 5, 4),
+                new GiantTrunkPlacer(5, 2, 4),
 
                 BlockStateProvider.simple(BlockInit.MAPLE_LEAVES.get()),
-                new BlobFoliagePlacer(ConstantInt.of(1), ConstantInt.of(1), 5),
+                new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(2), 4),
 
-                new TwoLayersFeatureSize(1, 0, 2)).build());
+                new TwoLayersFeatureSize(2, 1, 2)).build());
 
 
 
