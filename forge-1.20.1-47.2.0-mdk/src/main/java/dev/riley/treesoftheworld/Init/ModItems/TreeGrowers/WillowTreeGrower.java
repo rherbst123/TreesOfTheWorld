96518e4a36de92c1a0bb245ed794af25a13d2858
Riley Herbst <57201041+rherbst123@users.noreply.github.com>
2023-11-27 19:33:27 -0600

    Added Willow Trees

--- forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/treesoftheworld/Init/ModItems/TreeGrowers/WillowTreeGrower.java
+++ forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/treesoftheworld/Init/ModItems/TreeGrowers/WillowTreeGrower.java
@@ -1,5 +1,6 @@
 package dev.riley.treesoftheworld.Init.ModItems.TreeGrowers;
 
+import dev.riley.treesoftheworld.Init.ModItems.ModConfiguredFeatures;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
@@ -11,6 +12,6 @@ public class WillowTreeGrower extends AbstractTreeGrower {
     @Nullable
     @Override
     protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull RandomSource randomSource, boolean bool) {
-        return null;
+        return ModConfiguredFeatures.WILLOW;
     }
 }
